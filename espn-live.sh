edfa7a246e32c4bcbefcf01502c2b5b16d53e6ef
sudobum <35852808+sudobum@users.noreply.github.com>
2021-04-27 18:25:28 -0400

    Update espn-live.sh

--- espn-live.sh
+++ espn-live.sh
@@ -1,2 +1,10 @@
 #! /bin/sh
-ffmpeg -i "https://gma2.blab.email/fs2.m3u8"  -c:a aac -movflags +faststart ~/Desktop/ESPN-LIVE.mkv
+
+CNBC
+https://1674331492.rsc.cdn77.org/cqfMiWrVegJ0YE_MgseKfQ==,1619554305/LS-ATL-54548-14/tracks-v1a1/mono.m3u8
+
+ESPN
+https://gma2.blab.email/fs2.m3u8
+
+BLOOMBERG-TV-USA
+https://www.bloomberg.com/media-manifest/streams/phoenix-us.m3u8
