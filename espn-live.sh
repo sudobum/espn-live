b8b11e2bf5a0149822edcb33eca1d4f3fd9cfcb7
sudobum <35852808+sudobum@users.noreply.github.com>
2021-04-27 18:25:46 -0400

    Update espn-live.sh

--- espn-live.sh
+++ espn-live.sh
@@ -1,10 +1,7 @@
 #! /bin/sh
 
-CNBC
-https://1674331492.rsc.cdn77.org/cqfMiWrVegJ0YE_MgseKfQ==,1619554305/LS-ATL-54548-14/tracks-v1a1/mono.m3u8
 
 ESPN
 https://gma2.blab.email/fs2.m3u8
 
-BLOOMBERG-TV-USA
-https://www.bloomberg.com/media-manifest/streams/phoenix-us.m3u8
+
