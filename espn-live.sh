35b6970565a258e19efdc7fb5388f1b218e9c055
sudobum <35852808+sudobum@users.noreply.github.com>
2021-05-01 21:54:31 -0400

    Update espn-live.sh

--- espn-live.sh
+++ espn-live.sh
@@ -9,3 +9,8 @@ F1 qualifiying
 magnet:?xt=urn:btih:36d3f9f59ab12a53fa10571bb24a6f2b594f7475&dn=F1.2021.R03.Portuguese.Grand.Prix.Weekend.SkyF1HD.1080P&xl=24500313622&tr=udp%3A%2F%2Ftracker.coppersurfer.tk:6969/announce&tr=udp%3A%2F%2Ftracker.pirateparty.gr:6969/announce&tr=udp%3A%2F%2Fexodus.desync.com:6969/announce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org:6969/announce&tr=udp%3A%2F%2Ftracker.opentrackr.org:1337/announce&tr=udp%3A%2F%2Fopen.stealth.si/announce&tr=udp%3A%2F%2Fopen.demonii.si:1337/announce&tr=udp%3A%2F%2Fdenis.stalker.upeer.me:6969/announce&tr=udp%3A%2F%2Fexplodie.org:6969/announce&tr=udp%3A%2F%2Fipv4.tracker.harry.lu/announce&tr=udp%3A%2F%2Ftracker.internetwarriors.net:1337/announce&tr=udp%3A%2F%2Ftracker.dler.org:6969/announce&tr=udp%3A%2F%2Ftracker.torrent.eu.org:451/announce
 
 
+ESPN2
+https://gma2.blab.email/espn2.m3u8
+
+
+
